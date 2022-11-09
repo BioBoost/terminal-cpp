e83cbc5dd2ee48561dd4a95a1174b05d96e3f4da
Nico De Witte <nico.dewitte@outlook.com>
2022-11-09 17:57:26 +0100

    Allow key hits to be detected

--- main.cpp
+++ main.cpp
@@ -32,5 +32,25 @@ int main() {
   Terminal::reset_foreground_color();
   Terminal::clear();
 
+  cout << "Let's wait for some keypresses. Hit CTRL-C to stop." << endl;
+
+  while (true) {
+    Terminal::Key key = Terminal::get_key_press();
+    if (key != Terminal::Key::NONE) {
+      Terminal::clear();
+      switch (key) {
+        case Terminal::Key::LEFT: cout << "You pressed LEFT" << endl; break;
+        case Terminal::Key::RIGHT: cout << "You pressed RIGHT" << endl; break;
+        case Terminal::Key::UP: cout << "You pressed UP" << endl; break;
+        case Terminal::Key::DOWN: cout << "You pressed DOWN" << endl; break;
+        case Terminal::Key::ENTER: cout << "You pressed ENTER" << endl; break;
+        case Terminal::Key::SPACE: cout << "You pressed SPACE" << endl; break;
+        case Terminal::Key::ESC: cout << "You pressed ESC" << endl; break;
+      }
+    }
+
+    if (key == Terminal::Key::CTRL_C) exit(0);
+  }
+
   return 0;
 }
